ae9ed324d95d76017174b524785f8ebf3ccf732e
AD41934 <someshwaran.palani@lumen.com>
2026-01-03 10:04:58 +0530

    Update capacitor config app name to Somu Fin - Tracker

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
     appId: 'com.fintrack.pro',
-    appName: 'FinTrack Pro',
+    appName: 'Somu Fin - Tracker',
     webDir: 'dist',
     server: {
         androidScheme: 'https'
